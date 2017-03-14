fb3ba5a90b2f0bf89a47207b1cc6d21121dbfcba
Factory <poqi7639@naver.com>
2017-03-14 16:41:26 +0900

    Add files via upload

---
+++ src/test.py
@@ -0,0 +1,13 @@
+﻿word = ['sangmyung university', 'university']
+
+d = dict()
+for e in word:
+    for w in e.split(): 
+        if w not in d:
+            d[w]=1
+        else:
+            d[w]=d[w]+1
+print "key_value",d 
+print "save number ", len(d)
+print "key: ",d.keys()
+print "value:", d.values()
