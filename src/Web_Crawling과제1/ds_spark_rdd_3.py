8a4a08e505233c4f2b023adcd1b867ad6430125f
KYC <poqi7639@naver.com>
2017-05-29 12:23:48 +0900

    Add files via upload

---
+++ src/Web_Crawling과제1/ds_spark_rdd_3.py
@@ -0,0 +1,29 @@
+import os
+import sys 
+
+os.environ["SPARK_HOME"]="C:\Users\poqi7\Desktop\Code\s-201211260\src\spark-2.0.0-bin-hadoop2.7"
+os.environ["PYLIB"]=os.path.join(os.environ["SPARK_HOME"],'python','lib')
+sys.path.insert(0,os.path.join(os.environ["PYLIB"],'py4j-0.10.1-src.zip'))
+sys.path.insert(0,os.path.join(os.environ["PYLIB"],'pyspark.zip'))
+
+import pyspark
+myConf=pyspark.SparkConf()
+spark = pyspark.sql.SparkSession.builder.master("local").appName("myApp").config(conf=myConf).config('spark.sql.warehouse.dir','file:///C:\Users\PC\Code\s-201211299\data').getOrCreate()
+
+documents = spark.sparkContext.textFile("data/ds_spark_wiki.txt").map(lambda line: line.split(" "))
+
+from operator import add
+wc = spark.sparkContext.textFile("data/ds_spark_wiki.txt")\
+    .map(lambda x: x.replace(',',' ').replace('.',' ').replace('-',' ').lower())\
+    .map(lambda x:x.split())\
+    .map(lambda x:[(i,1) for i in x])
+
+for e in wc.collect():
+    print e
+documents = spark.sparkContext.textFile("data/ds_spark_wiki.txt")\
+    .map(lambda line: line.split(" "))
+from pyspark.mllib.feature import HashingTF
+
+hashingTF = HashingTF()
+tf = hashingTF.transform(documents)
+tf.collect()
